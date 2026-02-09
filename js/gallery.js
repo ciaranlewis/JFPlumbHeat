72d7b03985874217b51558854a48abf4c3853d6b
Ciaran <ciaranlewis20@gmail.com>
2026-02-09 18:10:37 +0000

    update

--- js/gallery.js
+++ js/gallery.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
   });
   
   function loadAllGalleries() {
-    fetch('/static/gallery/media.json')
+    fetch('media.json')
       .then(res => res.json())
       .then(data => {
         Object.entries(data).forEach(([galleryId, files]) => {
